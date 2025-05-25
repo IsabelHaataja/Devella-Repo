16eb008fa6ed23f5d91b55036edc5b02cedc0891
Isabel Haataja <isabelhaataja@gmail.com>
2025-05-25 15:36:05 +0200

    Added initials helper and enum helper class that extracts the display name for the UI to use

---
+++ DevellaSolution/DevellaLib/Helpers/InitialsHelper.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DevellaLib.Helpers;
+
+public static class InitialsHelper
+{
+    public static string GetInitials(string firstName, string surname)
+    {
+        var fullName = $"{firstName} {surname}".Trim();
+        if (string.IsNullOrWhiteSpace(fullName))
+            return string.Empty;
+
+        var nameParts = fullName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return string.Concat(nameParts.Select(n => char.ToUpper(n[0])));
+    }
+}
